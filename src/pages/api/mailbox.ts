15e63ef79eb8f63fbee7658f366c54a21b95dc1a
mac <macdev2001@gmail.com>
2023-11-11 19:36:58 +0700

    mailbox api added and better without eslint

--- src/pages/api/mailbox.ts
+++ src/pages/api/mailbox.ts
@@ -3,19 +3,22 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
-    try {
-      const { content } = req.body;
+  try {
+    const { content, name = "anonymous" } = req.query;
+
+    if (req.method === 'POST') {
       const mail = await prisma.mailbox.create({
         data: {
           content,
+          name,
         },
       });
       res.status(201).json(mail);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' });
     }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
