3d823363765bbdae5e48d95f70be535f61a510a3
bluestar <canikissfemboy@gmail.com>
2023-11-18 21:13:11 +0700

    huh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,69 +7,22 @@ export default function Home() {
         <title>10near project</title>
         <meta name="description" content="The 10near project" />
       </Head>
-      <main className="flex min-h-screen w-full flex-col items-center justify-center bg-gradient-to-r from-pink-900 to-blue-900 py-4 text-gray-100">
-        <div className="hero flex h-screen w-full items-center justify-center">
+      <main className="min-h-screen w-full flex flex-col items-center justify-center bg-gradient-to-r from-pink-900 to-blue-900 py-4 text-gray-100">
+        <div className="hero h-screen w-full flex items-center justify-center">
           <div className="hero-content text-center">
             <div className="max-w-md">
               <h1 className="mb-5 text-6xl font-bold lg:text-7xl">10NEAR</h1>
-              <p className="mb-5 text-gray-200">Coming soon</p>
+              <p className="mb-5 text-gray-200">
+                Coming soon
+              </p>
             </div>
           </div>
-        </div>
-        {/*}
-        <div className="mt-8">
-          <h3 className="mb-2 text-xl font-semibold">🔍 What We Do</h3>
-          <blockquote className="w-64 border-l-4 bg-pink-200 bg-opacity-0 border-blue-800 pl-2 text-gray-100 lg:w-96 lg:pl-4">
-            We conduct cybersecurity research, primarily focusing on
-            identifying vulnerabilities in client-side websites. Our research
-            includes uncovering issues in websites such as misconfigured
-            WordPress platforms, pre-built websites, and identifying
-            shortcomings in Thai government websites.
-          </blockquote>
-  </div> */}
+
+        </div> 
+
       </main>
     </>
   );
 }
 
-{
-  /*import Head from "next/head";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>10near project</title>
-        <meta name="description" content="The 10near project" />
-      </Head>
-      <main className="container min-h-screen w-auto flex flex-col items-center justify-center bg-gradient-to-r from-pink-900 to-blue-900 py-4">
-          <div className="hero min-h-screen">
-            <div className=""></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold text-gray-100 lg:text-7xl">
-                  10NEAR
-                </h1>
-                <p className="mb-5 text-gray-200">
-                  Project to bring Osama bin Laden back and beyond warcrime
-                  limitations
-                </p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <h3 className="mb-2 text-xl font-semibold">🔍 What We Do</h3>
-            <blockquote className="w-64 border-l-2 bg-pink-200 bg-opacity-0 border-blue-800 pl-1 text-gray-100 lg:w-96 lg:pl-4">
-              We conduct cybersecurity research, primarily focusing on
-              identifying vulnerabilities in client-side websites. Our research
-              includes uncovering issues in websites such as misconfigured
-              WordPress platforms, pre-built websites, and identifying
-              shortcomings in Thai government websites.
-            </blockquote>
-          </div>
-      </main>
-    </>
-  );
-}
-*/
-}
