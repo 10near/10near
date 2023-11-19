4c117e09abab9e23b027c1ca14252841516f702b
bluestar <canikissfemboy@gmail.com>
2023-11-19 11:37:16 +0700

    get better btw

--- src/pages/mailbox.tsx
+++ src/pages/mailbox.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
 import Head from "next/head";
-export default function Home() {
+
+export default function Mailbox() {
+  const [message, setMessage] = useState("");
+  const [statusMessage, setStatusMessage] = useState(null);
+
+  const handleSendMessage = async () => {
+    if (message.trim() === "") {
+      setStatusMessage("Please enter a message before sending.");
+      return;
+    }
+
+    if (message.length > 2000) {
+      setStatusMessage("Message is too long. Please keep it within 2000 characters.");
+      return;
+    }
+
+    try {
+      const response = await fetch(`/api/mail/send?content=${encodeURIComponent(message)}`, {
+        method: "POST",
+      });
+
+      if (response.ok) {
+        setStatusMessage("Message sent successfully!");
+        setMessage("");
+      } else {
+        setStatusMessage("Failed to send message. Please try again later.");
+      }
+    } catch (error) {
+      console.error("Error sending message:", error);
+      setStatusMessage("An unexpected error occurred. Please try again later.");
+    }
+  };
+
   return (
     <>
       <Head>
@@ -12,13 +45,21 @@ export default function Home() {
             <div className="label mb-4 text-2xl font-bold">
               Send any message to 10near
             </div>
-
-
             <textarea
               className="textarea textarea-bordered bg-transparent text-semibold text-gray-100 mb-2"
               placeholder="Your message here..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
-            <button className="btn btn-outline btn-block hover:bg-zinc-200">send</button>
+            <div className="p-2 text-bold">
+            {statusMessage && <p className={statusMessage.includes("success") ? "text-green-600" : "text-red-600"}>{statusMessage}</p>}
+            </div>
+            <button
+              className="btn btn-outline btn-block hover:bg-zinc-200"
+              onClick={handleSendMessage}
+            >
+              Send
+            </button>
           </div>
         </div>
       </main>
